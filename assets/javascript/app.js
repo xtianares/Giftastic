c1758c8e98357055a48d5a0f213a1cce4133f253
xtianares <xtianares@yahoo.com>
2019-01-08 22:57:47 -0500

    buttons rendering, api call, button trigger are all working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,59 @@
 // spinner during api call
-// destructure pai results
+// destructure api results
 // html templates for the gif elements variables
+
+var movies = ["The Matrix", "The Notebook", "Mr. Nobody", "The Lion King"];
+
+function renderButtons() {
+    $("#buttons-container").empty();
+    for (var i = 0; i < movies.length; i++) {
+        let btn = `
+                <button class="btn btn-success" type="button" data-name="${movies[i]}">${movies[i]}</button>
+            `;
+        $("#buttons-container").append(btn);
+    }
+}
+
+function displayGifs() {
+    let movie = $(this).attr("data-name"),
+        api_key = "2waeg1EgKzge3SHpASXilZ93joi92FC2",
+        queryURL = "https://api.giphy.com/v1/gifs/search?q=" + movie + "&api_key=" + api_key + "&limit=10";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+    .done(function(results) {
+        let data = results.data;
+        console.log(data);
+
+        for (var i = 0; i < data.length; i++) {
+            let { rating, images: {fixed_width} } = data[i];
+            let gifDiv = `
+                    <div class="card border-0">
+                        <img class="card-img-top" src="${fixed_width.url}">
+                        <div class="card-body">
+                            <p class="card-text">Rating: ${rating}</p>
+                        </div>
+                    </div>
+                `;
+
+            $("#gifs-container").prepend(gifDiv);
+        }
+    })
+    .fail(function(err) {
+        throw err;
+    });
+}
+// trigger to display the gifs
+$('#buttons-container').on('click', 'button', displayGifs);
+
+// This function handles events where a movie button is clicked
+$("#add-button").on("click", function(event) {
+    event.preventDefault();
+    let movie = $("#search-input").val().trim();
+    movies.push(movie);
+    renderButtons();
+});
+
+renderButtons();
