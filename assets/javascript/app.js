1edf013dc8b7813e20cf150813d359d7e0a6f9fb
xtianares <xtianares@yahoo.com>
2019-01-13 01:11:20 -0500

    added liked functionality via localStorage

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,12 @@
 let heroes = ["Superman", "Batman", "Deadpool", "Black Panther", "Iron Man", "Captain America", "Wonder Woman", "Aquaman"];
 
 let currentLimit = 0,
-    lastQuery = '';
+    lastQuery = '',
+    liked = [];
+
+if (localStorage.getItem('liked')) {
+    liked = JSON.parse(localStorage.getItem('liked'));
+}
 
 function renderButtons() {
     $("#buttons-container").empty();
@@ -33,13 +38,22 @@ function displayGifs(query, showMore) {
         let gifDiv = '';
         console.log(data);
         for (var i = 0 + currentLimit; i < data.length; i++) {
-            let { rating, embed_url, images: {fixed_width_still, fixed_width} } = data[i];
+            let { id, title, rating, embed_url, images: {fixed_width_still, fixed_width} } = data[i];
             gifDiv += `
                     <div class="card border-0">
                         <img class="card-img-top gif" src="${fixed_width_still.url}" data-paused="${fixed_width_still.url}"  data-play="${fixed_width.url}" data-state="paused">
                         <div class="card-body">
+                `;
+            if (liked.indexOf(id) > -1) {
+                gifDiv += `<a href="#" class="fave-icon liked float-right" data-id="${id}"></a>`;
+            }
+            else {
+                gifDiv += `<a href="#" class="fave-icon float-right" data-id="${id}"></a>`;
+            }
+            gifDiv += `
                             <p class="card-text">Rating: <span class="rating-value">${rating}</span></p>
-                            <p class="card-text">Embed URL: <a class="embed-url" href="${embed_url}" target="_blank">${embed_url}</a></p>
+                            <p class="card-text">Title:<br>${title}</p>
+                            <p class="card-text">Embed URL:<br><a class="embed-url" href="${embed_url}" target="_blank">${embed_url}</a></p>
                         </div>
                     </div>
                 `;
@@ -64,18 +78,21 @@ function displayGifs(query, showMore) {
 
 // trigger to display the gifs
 $('#buttons-container').on('click', 'button', function() {
+    event.preventDefault();
     let query = $(this).attr("data-name");
     $("#buttons-container button").removeAttr("disabled");
     $(this).attr("disabled", "disabled");
     displayGifs(query);
 });
 $('#show-more').on('click', 'button', function() {
+    event.preventDefault();
     let query = $(this).attr("data-name");
     displayGifs(query, true);
 });
 
 // triggger to play/pause Gifs
 $("#gifs-container").on("click", "img.gif", function() {
+    event.preventDefault();
     var state = $(this).data("state");
     if (state === "paused") {
         $(this).attr("src", $(this).data("play"));
@@ -85,6 +102,24 @@ $("#gifs-container").on("click", "img.gif", function() {
         $(this).data("state", "paused");
     }
 });
+
+// favorite icon
+$('#gifs-container').on('click', '.fave-icon', function() {
+    event.preventDefault();
+    let gifID = $(this).data("id")
+    $(this).toggleClass('liked');
+    if (liked.indexOf(gifID) < 0) {
+        liked.push(gifID);
+    }
+    else {
+        var index = liked.indexOf(gifID);
+        if (index > -1) {
+            liked.splice(index, 1);
+        }
+    }
+    localStorage.setItem('liked', JSON.stringify(liked))
+});
+
 
 // function to add more buttons
 $("#add-button").on("click", function(event) {
